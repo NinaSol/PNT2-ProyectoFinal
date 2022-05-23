15bbec8e6af0cee75b0097ad14a99530ad49f306
NinaSol <ninamarotta18@gmail.com>
2022-05-23 18:59:20 -0500

    display animal cards from api and its filters

---
+++ pnt2-adopcion/src/components/AnimalEnAdopcionCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card" style="width: 16rem">
+    <div style="width: 18rem">
+      <img
+        src="https://www.diarioveterinario.com/images/showid2/3192567?w=900"
+        class="card-img-top"
+        alt=""
+        style="width: 16rem"
+      />
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">{{ nombre }}, {{ edad }}, {{ raza }}</h5>
+      <p class="card-text">
+        {{ comentarios }}
+      </p>
+      <button
+        href="#"
+        class="btn btn-details"
+        style="color: white; background-color: #49ff00"
+      >
+        Quiero ver mas
+      </button>
+    </div>
+  </div>
+  <!-- <router-view></router-view>
+    <button type="button" class="btn btn-primary mt-2" @click="navigateToHome">
+      Volver a Home
+    </button>-->
+</template>
+
+<script>
+export default {
+  name: "AnimalEnAdopcionCard",
+  props: {
+    nombre: String,
+    raza: String,
+    edad: Number,
+    comentarios: String,
+  },
+  methods: {
+    navigateToHome() {
+      console.log("en este momento podemos realizar una o varias acciones...");
+      this.$router.push("/home");
+    },
+  },
+};
+</script>
+
+<style>
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+</style>
---
+++ pnt2-adopcion/src/components/AnimalesEnAdopcion.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="setEspecie('perro')">perros</button>
+    <button class="btn btn-primary" @click="setEspecie('gato')">gatos</button>
+    <button class="btn btn-primary" @click="setEspecie('otro')">otros</button>
+    <button class="btn btn-primary" @click="setEspecie('todos')">todos</button>
+
+    <div class="container">
+      <div class="row">
+        <div
+          class="pb-4 col-lg-3 col-sm-6 col-xs-12"
+          v-for="mascota in mascotasFiltradas"
+          :key="mascota.id"
+        >
+          <AnimalEnAdopcionCard
+            :nombre="mascota.nombre"
+            :raza="mascota.raza"
+            :comentarios="mascota.comentarios"
+            :edad="mascota.edad"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AnimalEnAdopcionCard from "./AnimalEnAdopcionCard.vue";
+
+export default {
+  name: "AnimalesEnAdopcion",
+  components: {
+    AnimalEnAdopcionCard,
+  },
+  data() {
+    return {
+      especie: null,
+      mascotas: [],
+      mascotasFiltradas: [],
+      url: "https://6282faeb92a6a5e4621c22e0.mockapi.io/pnt2/mascota",
+    };
+  },
+  methods: {
+    navigateToHome() {
+      this.$router.push("/home");
+    },
+    setEspecie(value) {
+      this.especie = value;
+    },
+  },
+  async created() {
+    const response = await fetch(this.url);
+    const results = await response.json();
+    this.mascotas = results;
+    //this.mascotasFiltradas = results;
+  },
+  watch: {
+    mascotas: function () {
+      this.mascotasFiltradas = this.mascotas;
+    },
+    especie: function () {
+      if (this.especie === "todos") {
+        this.mascotasFiltradas = this.mascotas;
+      } else {
+        this.mascotasFiltradas = this.mascotas.filter((m) =>
+          m.especie.toLowerCase().includes(this.especie.toLowerCase())
+        );
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+</style>
