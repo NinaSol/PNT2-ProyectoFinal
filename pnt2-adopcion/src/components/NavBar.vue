f8c896e5d4ab95323b79316722efa18a49a5607e
Mauro Candotti <cmduilio@gmail.com>
2022-05-23 20:43:36 -0300

    navbar wip

--- pnt2-adopcion/src/components/NavBar.vue
+++ pnt2-adopcion/src/components/NavBar.vue
@@ -1,8 +1,34 @@
 <template>
-  <div></div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Mis Mascotas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/solicitudes">Solicitudes</a>
+        </li>
+      </ul>
+      
+      <div>
+        <a class="nav-link" href="/solicitudes">Login</a>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.nav-right{
+  float: right;
+}
+.mr-auto{
+  margin-right: auto;
+}
+
+</style>
