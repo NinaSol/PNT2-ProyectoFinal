30db7785b795d9bb7d9cce1b4354050bcc4614f5
Mauro Candotti <cmduilio@gmail.com>
2022-06-06 20:59:51 -0300

    style of request card

--- pnt2-adopcion/src/components/RequestCard.vue
+++ pnt2-adopcion/src/components/RequestCard.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="card" style="width: 16rem; padding-top: 10px; padding-bottom: 10px;">
-    <img
-      :src="image"
-      class="card-img-top"
-      alt=""
-      style="width: 10rem; max-height:10rem; object-fit: contain; margin-left: auto; margin-right: auto;"
-    />
+    <div class="pt-1">
+      <img
+        :src="image"
+        class="card-img-top"
+        alt=""
+        style="width: 15rem;"
+      />
+    </div>
     <div class="card-body">
-      <h5 class="card-title" style="text-align: left;">Solicitante: {{ requesterName }}</h5>
-      <h5 class="card-title" style="text-align: left;">Mascota Solicitada: {{ petName }}</h5>
-      <h5 class="card-title" style="text-align: left;">Estado: {{ status }}</h5>
+      <span class="card-title" style="text-align: left;">Solicitante: {{ requesterName }}</span>
+      <span class="card-title" style="text-align: left;">Mascota Solicitada: {{ petName }}</span>
+      <span class="card-title" style="text-align: left;">Estado: {{ status }}</span>
       <button
         href="#"
         class="btn btn-details"
-        style="color: white; background-color: #49ff00"
+        style="color: white; background-color: #439c1e"
       >
         Detalle
       </button>
@@ -22,14 +24,14 @@
     <div class="column" style="margin-bo" v-show="showCommands">
       <button
         class="btn btn-details"
-        style="color: white; background-color: red; margin-right:10px"
+        style="color: white; background-color: #E91224; margin-right:10px"
         v-on:click="onReject"
       >
         Rechazar
       </button>
       <button
         class="btn btn-details"
-        style="color: white; background-color: #49ff00; margin-left:10px"
+        style="color: white; background-color: #007AD9; margin-left:10px"
         v-on:click="onConfirm"
       >
         Confirmar
@@ -66,4 +68,16 @@ export default {
 </script>
 
 <style>
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+.card-img-top {
+  border-radius: 2.5px;
+}
+.card {
+  box-shadow: 0px 0px 21px 3px rgba(0, 0, 0, 0.23);
+}
 </style>
