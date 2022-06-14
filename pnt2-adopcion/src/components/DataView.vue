a941174a90ed8b800efbc84568bc7c9dbe345696
facusafe <facundosafe10@gmail.com>
2022-06-13 21:37:52 -0300

    agrego componente padre

---
+++ pnt2-adopcion/src/components/DataView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md">
+        <div class="card mt-3">
+          <PetsPie />
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="card mt-3">
+          <RequestsLine />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PetsPie from "@/components/PetsPie";
+import RequestsLine from "@/components/RequestsLine";
+
+export default {
+  components: {
+    PetsPie,
+    RequestsLine,
+  },
+};
+</script>
+
+<style></style>
