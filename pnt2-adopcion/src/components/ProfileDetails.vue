2e81d18e670af828afa5410935e9e79cb5d8ac0a
facusafe <facundosafe10@gmail.com>
2022-06-13 22:44:08 -0300

    agrego componente padre e hijo

---
+++ pnt2-adopcion/src/components/ProfileDetails.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-lg-6 mb-4 mb-lg-0">
+        <div class="card mb-3" style="border-radius: 0.5rem">
+          <div class="row g-0">
+            <div
+              class="col-md-4 gradient-custom text-center"
+              style="
+                border-top-left-radius: 0.5rem;
+                border-bottom-left-radius: 0.5rem;
+              "
+            >
+              <img
+                src="../assets/default-user-icon.jpg"
+                alt="Avatar"
+                class="img-fluid my-5"
+                style="width: 80px"
+              />
+              <h4>{{ user.userName }}</h4>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body p-4">
+                <h5>Tu Perfil</h5>
+                <hr class="mt-0 mb-4" />
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Nombre</h6>
+                    <p class="text-muted">
+                      {{ user.name + " " + user.lastName }}
+                    </p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Teléfono</h6>
+                    <p class="text-muted">{{ user.phoneNumber }}</p>
+                  </div>
+                </div>
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Dirección</h6>
+                    <p class="text-muted">{{ user.address }}</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Email</h6>
+                    <p class="text-muted">{{ user.email }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object,
+  },
+};
+</script>
+
+<style></style>
---
+++ pnt2-adopcion/src/components/ProfileView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <ProfileDetails
+          v-if="user"
+          :user="{
+            id: user.id,
+            name: user.name,
+            lastName: user.lastName,
+            userName: user.userName,
+            address: user.address,
+            phoneNumber: user.phoneNumber,
+            email: user.email,
+          }"
+        />
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileDetails from "@/components/ProfileDetails.vue";
+
+export default {
+  components: {
+    ProfileDetails,
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
+  async created() {
+    const request = await fetch(
+      "https://6282faeb92a6a5e4621c22e0.mockapi.io/pnt2/users?id=" +
+        localStorage.id
+    );
+    const response = await request.json();
+    this.user = response[0];
+    console.log(this.user);
+  },
+};
+</script>
+
+<style></style>
