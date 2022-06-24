699ad212e8700279bbbbab1e740afe164604301c
facusafe <facundosafe10@gmail.com>
2022-06-24 10:51:09 -0300

    agrego line chart

---
+++ pnt2-adopcion/src/components/LineChart.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="mt-3">
+    <h4>{{ title }}</h4>
+    <LineChartGenerator
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+    />
+  </div>
+</template>
+
+<script>
+import { Line as LineChartGenerator } from "vue-chartjs/legacy";
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+);
+
+export default {
+  name: "LineChart",
+  components: {
+    LineChartGenerator,
+  },
+  props: {
+    title: String,
+    chartId: {
+      type: String,
+      default: "line-chart",
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label",
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 400,
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [
+          "Enero",
+          "Febrero",
+          "Marzo",
+          "Abril",
+          "Mayo",
+          "Junio",
+          "Agosto",
+        ],
+        datasets: [
+          {
+            label: "Solicitudes realizadas",
+            backgroundColor: "#f87979",
+            data: [1, 0, 0, 2, 1, 3, 0],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+};
+</script>
